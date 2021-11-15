f098e6b5b89d9e77eb86745d4878d17f3001170b
kandys93 <kandys93237@gmail.com>
2021-11-16 00:02:28 +0100

    initial commit

---
+++ Providers/IDataProviders.cs
@@ -0,0 +1,17 @@
+﻿using Phonebook.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Phonebook.Providers
+{
+    public interface IDataProvider
+    {
+        // type de retour/methode c'est un IEnumerable d'Entity qu'on va appeler List()
+        IEnumerable<Entity> List();
+        //type de retour/methode c'est un IEnumerable d'Entity qu'on va appeler Search(string text)
+        IEnumerable<Entity> Search(string text);
+    }
+}
